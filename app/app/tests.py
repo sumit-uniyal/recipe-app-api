6013a519e6b334ef8ea4776104dafe48654a0a48
Sumit uniyal <suniyal141@gmail.com>
2024-03-13 20:28:40 +0530

    new

---
+++ app/app/tests.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_number(self):
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        res = calc.subtract(10,15)
+        self.assertEqual(res, 5)
